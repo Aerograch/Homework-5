483f9f4be05da6d8afaf03a456a1d266e13100aa
Aerograch <aerograchalr@gmail.com>
2021-11-02 14:20:40 +0300

    Initial commit

---
+++ Single/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Single
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int len = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(' ');
+            foreach(string s in input)
+            {
+                if(Array.IndexOf(input, s) == Array.LastIndexOf(input, s))
+                {
+                    Console.Write($"{s} ");
+                }
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sorting/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int len = int.Parse(Console.ReadLine());
+            int[] input = Array.ConvertAll<string, int>(Console.ReadLine().Split(), new Converter<string, int>(Convert.ToInt32)); //Я решил покопаться в документации и поэксперементировать со встроенными функциями сортировки
+            int a;
+            for (int i = 1; i < len; i++)
+            {
+                for (int j = 0; j < len-i; j++)
+                {
+                    if(input[j] > input[j + 1])
+                    {
+                        a = input[j];
+                        input[j] = input[j + 1];
+                        input[j + 1] = a;
+                    }
+                }
+            }
+            foreach(int i in input)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
