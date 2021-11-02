cbb5697a0808c42b6e45d28364d961338d48094e
Aerograch <aerograchalr@gmail.com>
2021-11-02 14:51:48 +0300

    First Commit

---
+++ Power of M/Power of M.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Power_of_M
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int power = m;
+            while (true)
+            {
+                if (power == n)
+                {
+                    Console.WriteLine("YES");
+                    break;
+                }
+                if (power*m < 0)
+                {
+                    Console.WriteLine("NO");
+                    break;
+                }
+                power = power * m;
+            }
+        }
+    }
+}
